d50293728910e089803041d9cfdf88258a986f7f
Henry korir <longkhery@gmail.com>
2020-03-07 18:10:18 +0300

    Updates

---
+++ precache-manifest.bb36c66ef54f3e873121bc8f615d9800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6835fe340a0f2d50fe40f2fc754306",
+    "url": "/kipsaiya/index.html"
+  },
+  {
+    "revision": "400f3319c0e511e19841",
+    "url": "/kipsaiya/static/css/main.4fbc10c4.chunk.css"
+  },
+  {
+    "revision": "39991a295e6a81eac042",
+    "url": "/kipsaiya/static/js/2.0a42cd93.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kipsaiya/static/js/2.0a42cd93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400f3319c0e511e19841",
+    "url": "/kipsaiya/static/js/main.cbb7e6ff.chunk.js"
+  },
+  {
+    "revision": "5c5cef64bb2e4419a8ab",
+    "url": "/kipsaiya/static/js/runtime-main.ddd835d3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/kipsaiya/static/media/logo.ee7cd8ed.svg"
+  }
+]);
